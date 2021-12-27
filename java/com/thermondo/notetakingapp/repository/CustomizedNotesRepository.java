21641aee439afdb1347387c09d83f08f4448e4d6
DevarajBc <95009441+DevarajBc@users.noreply.github.com>
2021-12-27 17:30:24 +0530

    Delete java/com/thermondo/notetakingapp directory

--- java/com/thermondo/notetakingapp/repository/CustomizedNotesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thermondo.notetakingapp.repository;
-
-import com.thermondo.notetakingapp.model.entities.Note;
-
-import java.util.List;
-
-public interface CustomizedNotesRepository {
-    List<Note> search(String keyword);
-}
